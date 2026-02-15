3b418d2494a1e86841c6639a9bf49fa4786c312e
Your Name <you@example.com>
2026-02-15 19:43:30 +0530

    15Feb2026 19:44

--- client-portal/vite.config.ts
+++ client-portal/vite.config.ts
@@ -9,11 +9,19 @@ export default defineConfig({
     base: '/client-portal/',     // important: assets and index under /client-portal
   server: {
     port: 5174, // Different port from main app
-    host: true,
+    host: "::",
+        allowedHosts: [
+      "mudhro-frontend-e5a8gbbkc5f8buae.centralindia-01.azurewebsites.net/client-portal/",
+      "https://mudhrofrontend-duhqdhfbfahjbjaj.centralindia-01.azurewebsites.net/client-portal/",
+      "mudhro.com/client-portal/",
+      "www.mudhro.com/client-portal/",
+      "https://mudhro.com/client-portal/",
+      "https://www.mudhro.com/client-portal/"
+    ],
   },
    resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
+      '@': path.resolve(__dirname, './src')
     }
   },
   build: {
