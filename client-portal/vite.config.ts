e62ea4d52cdf1fef969a30d65083d579eb307f0d
Your Name <you@example.com>
2026-02-15 19:20:00 +0530

    15Feb2026 19:19

--- client-portal/vite.config.ts
+++ client-portal/vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +10,11 @@ export default defineConfig({
   server: {
     port: 5174, // Different port from main app
     host: true,
+  },
+   resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
   },
   build: {
     outDir: 'dist',
