9733ba41d7a356abd34286c4e656a38426228efa
Your Name <you@example.com>
2026-02-15 19:31:26 +0530

    15Feb2026 19:31

--- client-portal/src/lib/services/types.ts
+++
@@ -1,9 +0,0 @@
-// File storage types
-export enum FileType {
-  Logos = 'Logos',
-  Invoices = 'Invoices',
-  Expenses = 'Expenses',
-  ClientDocuments = 'ClientDocuments',
-  VendorDocuments = 'VendorDocuments',
-}
-
